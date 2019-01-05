a1fc36a6257e1236321a301b6003366d765f3dc2
Mauricio Flores Jr <floresmauricio14@gmail.com>
2019-01-05 00:08:25 -0600

    Initial commit

---
+++ app/src/main/java/com/mauriciofloresjr/whataretheodds/MainActivity.java
@@ -0,0 +1,60 @@
+package com.mauriciofloresjr.whataretheodds;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String ODDS_GUESS = "odds_guess";
+    private static final String ODDS_CHANCE = "odds_chance";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnFunc(View view) {
+
+        // Creating toasts for error handling
+        Toast numError = Toast.makeText(this, "Numbers cannot be lower than 1!", Toast.LENGTH_SHORT);
+        Toast guessOutOfRange = Toast.makeText(this,"Guess cannot exceed odds!", Toast.LENGTH_SHORT);
+        Toast numOutOfRange = Toast.makeText(this,"Your inputs cannot go over 100000!", Toast.LENGTH_SHORT);
+        Toast inputMissing = Toast.makeText(this,"You must put a number in every field!", Toast.LENGTH_SHORT);
+
+        //Finding all text views for user input
+        EditText oddsChance = findViewById(R.id.oddsChanceTV);
+        EditText oddsGuess = findViewById(R.id.oddsGuessTV);
+
+        // Creating the intent to switch activities
+        Intent results = new Intent(this, ResultActivity.class);
+
+        // Transfers user input strings to integers to prepare for data transfer to results activity
+        String oddsChanceStr = oddsChance.getText().toString();
+        int oddsChanceNum = Integer.parseInt(oddsChanceStr);
+
+        String oddsGuessStr = oddsGuess.getText().toString();
+        int oddsGuessNum = Integer.parseInt(oddsGuessStr);
+
+        // Setting if statement to check if user has numbers greater than 1
+        if (oddsGuessNum < 1 || oddsChanceNum < 1) {
+            numError.show();
+        } else if (oddsChanceStr.isEmpty() || oddsGuessStr.isEmpty()) {
+            inputMissing.show();
+        } else if (oddsGuessNum > oddsChanceNum) {
+            guessOutOfRange.show();
+        } else if (oddsGuessNum > 100000 || oddsChanceNum > 100000) {
+            numOutOfRange.show();
+        } else {
+            results.putExtra(ODDS_CHANCE, oddsChanceNum);
+            results.putExtra(ODDS_GUESS, oddsGuessNum);
+            startActivity(results);
+        }
+    }
+}
---
+++ app/src/main/java/com/mauriciofloresjr/whataretheodds/ResultActivity.java
@@ -0,0 +1,71 @@
+package com.mauriciofloresjr.whataretheodds;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Half;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private static final String ODDS_GUESS = "odds_guess";
+    private static final String ODDS_CHANCE = "odds_chance";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        showResults();
+    }
+    public void showResults(){
+        //Setting vars for storage and taking numbers from user input
+        int oddsChanceNum = getIntent().getIntExtra(ODDS_CHANCE, 0);
+        int oddsGuessNum = getIntent().getIntExtra(ODDS_GUESS, 0);
+
+        //Finding different TextViews
+        TextView resultsTitleTV2 = findViewById(R.id.resultsTitleTV2);
+        TextView resultsTitleTV4 = findViewById(R.id.resultsTitleTV4);
+        TextView resultsTV = findViewById(R.id.resultsTV);
+        TextView statView = findViewById(R.id.statView1);
+
+        //Creating a decimal format for formatting percentage number
+        DecimalFormat precision = new DecimalFormat("###.#####");
+
+        //Creating sounds
+        MediaPlayer victorySnd = MediaPlayer.create(this, R.raw.success_sound);
+        MediaPlayer failureSnd = MediaPlayer.create(this, R.raw.failure_sound);
+
+        //Creating the random number generator
+        Random random = new Random();
+        int randomNum;
+        randomNum = random.nextInt(oddsChanceNum) + 1;
+        String randomNumStr = String.valueOf(randomNum);
+
+        resultsTitleTV2.setText(getString(R.string.resultsText2, oddsChanceNum));
+        resultsTitleTV4.setText(getString(R.string.resultsText4,oddsGuessNum));
+        resultsTV.setText(randomNumStr);
+
+        //Creating a percentage calculator
+        double percentage = (1.000f / oddsChanceNum) * 100;
+
+        statView.setText(getString(R.string.percentOdds,precision.format(percentage)));
+
+        //Creating victory case
+        if (oddsGuessNum == randomNum) {
+            victorySnd.start();
+        } else {
+            failureSnd.start();
+        }
+    }
+
+    public void backBtn(View v){
+        finish();
+    }
+}
